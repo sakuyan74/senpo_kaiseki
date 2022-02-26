965dbc94530ff47ff69aaf72ab1bb1af6593f035
sakuyan74 <ginkou74@yahoo.co.jp>
2022-02-26 20:44:52 +0900

    プレフィックス変更

--- senpo_kaiseki/discordbot.py
+++ senpo_kaiseki/discordbot.py
@@ -71,46 +71,46 @@ class discordbot(commands.Bot):
                     db_client = MongoDatabaseClient()
                     success_friend, success_enemy = db_client.insert_data(result)
                     if success_friend == SuccessCode.FAILED:
-                        await message.channel.send("味方ユーザのデータ読み取りが失敗しました。")
+                        await message.channel.send("★味方ユーザのデータ読み取りが失敗しました。")
                     else:
                         if success_friend == SuccessCode.INSERT:
-                            await message.channel.send("以下の内容でデータベース登録が成功しました")
+                            await message.channel.send("★以下の内容でデータベース登録が成功しました")
                         if success_friend == SuccessCode.UPDATE:
-                            await message.channel.send("以下の内容でデータベース更新が成功しました")
+                            await message.channel.send("★以下の内容でデータベース更新が成功しました")
                         await message.channel.send("味方ユーザ：" + result[ResultCode.USER_NAME.name])
                         await message.channel.send("味方主将Lv：" + result[ResultCode.LEVEL_1.name] +
                                                    "\n味方副将1Lv：" + result[ResultCode.LEVEL_2.name] +
                                                    "\n味方副将2Lv：" + result[ResultCode.LEVEL_3.name])
                         await message.channel.send("味方主将戦法1：" + result[ResultCode.SENPO_1_1.name] +
-                                                   "\n味方主将戦法2：：" + result[ResultCode.SENPO_1_2.name] +
-                                                   "\n味方主将戦法3：：" + result[ResultCode.SENPO_1_3.name])
+                                                   "\n味方主将戦法2：" + result[ResultCode.SENPO_1_2.name] +
+                                                   "\n味方主将戦法3：" + result[ResultCode.SENPO_1_3.name])
                         await message.channel.send("味方副将1戦法1：" + result[ResultCode.SENPO_2_1.name] +
-                                                   "\n味方副将1戦法2：：" + result[ResultCode.SENPO_2_2.name] +
-                                                   "\n味方副将1戦法3：：" + result[ResultCode.SENPO_2_3.name])
+                                                   "\n味方副将1戦法2：" + result[ResultCode.SENPO_2_2.name] +
+                                                   "\n味方副将1戦法3：" + result[ResultCode.SENPO_2_3.name])
                         await message.channel.send("味方副将2戦法1：" + result[ResultCode.SENPO_3_1.name] +
-                                                   "\n味方副将2戦法2：：" + result[ResultCode.SENPO_3_2.name] +
-                                                   "\n味方副将2戦法3：：" + result[ResultCode.SENPO_3_3.name])
+                                                   "\n味方副将2戦法2：" + result[ResultCode.SENPO_3_2.name] +
+                                                   "\n味方副将2戦法3：" + result[ResultCode.SENPO_3_3.name])
                     if success_enemy == SuccessCode.FAILED:
-                        await message.channel.send("味方ユーザのデータ読み取りが失敗しました。")
+                        await message.channel.send("★敵ユーザのデータ読み取りが失敗しました。")
                     else:
                         if success_enemy == SuccessCode.INSERT:
-                            await message.channel.send("以下の内容でデータベース登録が成功しました")
+                            await message.channel.send("★以下の内容でデータベース登録が成功しました")
                         if success_enemy == SuccessCode.UPDATE:
-                            await message.channel.send("以下の内容でデータベース更新が成功しました")
+                            await message.channel.send("★以下の内容でデータベース更新が成功しました")
                         await message.channel.send("敵ユーザ：" + result[ResultCode.E_USER_NAME.name] +
                                                    "\n敵同盟：" + result[ResultCode.E_ALLIANCE_NAME.name])
                         await message.channel.send("敵主将Lv：" + result[ResultCode.E_LEVEL_1.name] +
                                                    "\n敵副将1Lv：" + result[ResultCode.E_LEVEL_2.name] +
                                                    "\n敵副将2Lv：" + result[ResultCode.E_LEVEL_3.name])
                         await message.channel.send("敵主将戦法1：" + result[ResultCode.E_SENPO_1_1.name] +
-                                                   "\n敵主将戦法2：：" + result[ResultCode.E_SENPO_1_2.name] +
-                                                   "\n敵主将戦法3：：" + result[ResultCode.E_SENPO_1_3.name])
+                                                   "\n敵主将戦法2：" + result[ResultCode.E_SENPO_1_2.name] +
+                                                   "\n敵主将戦法3：" + result[ResultCode.E_SENPO_1_3.name])
                         await message.channel.send("敵副将1戦法1：" + result[ResultCode.E_SENPO_2_1.name] +
-                                                   "\n敵副将1戦法2：：" + result[ResultCode.E_SENPO_2_2.name] +
-                                                   "\n敵副将1戦法3：：" + result[ResultCode.E_SENPO_2_3.name])
+                                                   "\n敵副将1戦法2：" + result[ResultCode.E_SENPO_2_2.name] +
+                                                   "\n敵副将1戦法3：" + result[ResultCode.E_SENPO_2_3.name])
                         await message.channel.send("敵副将2戦法1：" + result[ResultCode.E_SENPO_3_1.name] +
                                                    "\n敵副将2戦法2：：" + result[ResultCode.E_SENPO_3_2.name] +
-                                                   "\n敵副将2戦法3：：" + result[ResultCode.E_SENPO_3_3.name])
+                                                   "\n敵副将2戦法3：" + result[ResultCode.E_SENPO_3_3.name])
 
             except Exception as e:
                 await message.channel.send('サーバ内部エラー：しばらくおまちください。')
